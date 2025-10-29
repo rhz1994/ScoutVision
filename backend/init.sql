8fafa6e04e1a2d96e3cc00ca32e85dff0bfb178a
Rasmus Hanzén <rhz1994@gmail.com>
2025-10-29 13:10:45 +0100

    added init.sql

---
+++ backend/init.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS user_reports CASCADE;
+DROP TABLE IF EXISTS reportedlinks CASCADE;
+DROP TABLE IF EXISTS reportedphonenumbers CASCADE;
+DROP TABLE IF EXISTS tests CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+
+CREATE TABLE users {
+  id serial PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT now()
+}
+
+CREATE TABLE reportedPhoneNumbers {
+  id serial PRIMARY KEY,
+  phone_number TEXT UNIQUE NOT NULL,
+  report_count INTEGER DEFAULT 1,
+  reporter_id INTEGER,
+  FOREIGN KEY (reporter_id) REFERENCES users(id)
+}
+
+CREATE TABLE reportedLinks {
+  id serial PRIMARY KEY,
+  link TEXT UNIQUE NOT NULL,
+  report_count INTEGER DEFAULT 1,
+  last_reported TIMESTAMP DEFAULT now(),
+  reporter_id INTEGER,
+  FOREIGN KEY (reporter_id) REFERENCES users(id)
+}
+
+CREATE TABLE tests {
+  id serial PRIMARY KEY,
+  type TEXT NOT NULL,
+  suspect_details TEXT NOT NULL,
+  result INTEGER NOT NULL,
+  created_at TIMESTAMP DEFAULT now()
+}
