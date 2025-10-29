15c6b80f05d7a2fd357bcaec637e1bb43d96e313
richardMattsson <mattsson.richard@gmail.com>
2025-10-29 14:43:32 +0100

    add endpoints in backend and fetch in frontend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,52 @@
-const express = require("express"),
-  path = require("path");
-
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const { Client } = require("pg");
 const app = express();
 
-app.get("/api", (_request, response) => {
-  response.send({ hello: "World" });
+dotenv.config();
+
+const client = new Client({
+  connectionString: process.env.PGURI,
+});
+
+client.connect();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+port = 3000;
+
+app.get("/api/userTests/:id", async (req, res) => {
+  const { id } = req.params;
+  const { rows } = await client.query(
+    `SELECT * FROM tests WHERE user_id = ${id};`
+  );
+  res.send(id);
+});
+
+app.post("/api/post", (req, res) => {
+  const body = req.body;
+
+  console.log(body);
+
+  res.send();
+});
+
+app.put("api/update/:id", (req, res) => {
+  const { id } = req.params;
+
+  console.log(id);
+
+  res.send();
+});
+
+app.delete("api/delete/:id", (req, res) => {
+  const { id } = req.params;
 });
 
 app.use(express.static(path.join(path.resolve(), "dist")));
 
-app.listen(3000, () => {
-  console.log("Redo på http://localhost:3000/");
+app.listen(port, () => {
+  console.log(`Redo på port http://localhost:${port}/`);
 });
